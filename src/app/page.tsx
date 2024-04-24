9adb88792c0d87bd27219a455cf29cd4a5f361ae
Jaredgg4 <jg70456898@gmail.com>
2024-04-24 13:07:51 -0600

    Added a setCount function so that the onClick function works as intended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,12 @@
-/*import React from 'react';
-interface UserProfileState {
-  age: number;
-}
-class UserProfile extends React.Component<any, UserProfileState> {
-  constructor(props: any){
-    super(props);
-    this.state = {
-      age: 0
-    };
-  }
+"use client";
 
-  celebrateBirthday = () => {
-    this.setState((prevState) => ({
-      age: prevState.age + 1
-    }));
-  }
+import React, { useState } from 'react';
+export default function UserProfile({ name = "George"}){
 
-  render() {
-    return (
-      <div>
-        <h1>{this.props.name} Profile</h1>
-        <p>Age: {this.state.age}</p>
-        <button>Happy Birthday!</button>
-      </div>
-    )
-  }
-}
+  const [age, setCount] = useState(0);
 
-export default UserProfile;*/
-'use client';
-export default function UserProfile({ age = 17, name = "George"}: any){
   function celebrateBirthday(): any{
-    return age = age + 1;
+    setCount(age + 1);
   }
 
   return (
